1cc47d64f94f83fd3e6e9205eab157838d882099
jobayer-alam-24 <158845902+jobayer-alam-24@users.noreply.github.com>
2024-12-12 17:31:36 +0600

    working

---
+++ Password_Implement/Controllers/AccountController.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Password_Implement.Models;
+
+namespace Password_Implement.Controllers
+{
+    public class AccountController : Controller
+    {
+        [Route("/Account/Login")]
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            return View(model);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
