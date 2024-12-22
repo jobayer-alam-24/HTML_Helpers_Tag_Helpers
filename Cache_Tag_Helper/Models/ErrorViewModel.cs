38783eac82999037db270fdefbb9a978542feaa9
jobayer-alam-24 <158845902+jobayer-alam-24@users.noreply.github.com>
2024-12-22 17:39:50 +0600

    working

---
+++ Cache_Tag_Helper/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Cache_Tag_Helper.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
